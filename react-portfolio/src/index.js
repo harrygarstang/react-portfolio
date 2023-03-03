d47b44c16dc351f3e1744bf0063bfa68d4d2170c
harryg <harry@gmail.com>
2023-03-03 19:57:59 +0000

    Added CSS file with initial styling of navbar. Debugging to index.js

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,6 +1,8 @@
+import './custom.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
