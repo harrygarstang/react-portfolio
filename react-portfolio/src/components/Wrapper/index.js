622faff4a4ffd0fbbccfea29b8991427a26d0953
harryg <harry@gmail.com>
2023-03-07 17:08:18 +0000

    Fixed issue of whitespace and layout. Implemented a wrapper that takes childen props and wraps everything in a container, as opposed to just props utilised previously.

--- react-portfolio/src/components/Wrapper/index.js
+++ react-portfolio/src/components/Wrapper/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style.css";
+import { Container } from 'react-bootstrap';
 
-
-function Wrapper(props) {
-    
-  return <main className="wrapper">{props.children}</main>;
+function Wrapper({ children }) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
 }
 
 export default Wrapper;
